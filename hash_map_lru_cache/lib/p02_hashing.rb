b319dbae1be3c3ebea9483317d0774fa57b31c5b
Adam Kamboj <kamboj.adam@gmail.com>
2021-01-25 15:22:13 -0500

    completed hashing

--- hash_map_lru_cache/lib/p02_hashing.rb
+++ hash_map_lru_cache/lib/p02_hashing.rb
@@ -4,11 +4,26 @@ end
 
 class Array
   def hash
+    total = 0
+    self.each_with_index { |ele, i| total += (ele.hash * i.hash) }
+    if self.size > 0
+      total/self.size
+    else
+      0.hash
+    end
   end
 end
 
 class String
   def hash
+    total = 0
+    # self.chars.map { |char| char.to_sym }
+    self.chars.map(&:to_sym).each_with_index { |ele, i| total += (ele.hash * i.hash) }
+    if self.size > 0
+      total/self.size
+    else
+      0.hash
+    end
   end
 end
 
@@ -16,6 +31,13 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    total = 0
+    self.to_a.sort.each_with_index { |ele, i| total += (ele.hash * i.hash) }
+    if self.size > 0
+      total/self.size
+    else
+      0.hash
+    end
   end
 end
+
